d4f36078f478dcd27c7ce7c615c783ba8cb25cc3
laryol <laryol2@gmx.de>
2022-02-03 17:14:16 +0100

    111

--- Project/Assets/Scripts/BlackCarAgent.cs
+++ Project/Assets/Scripts/BlackCarAgent.cs
@@ -44,6 +44,7 @@ public class BlackCarAgent : Agent{
         continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 //  Debug.Log(other.material.name);
+/*
      void OnCollisionEnter(Collision collision)
  {
          if (collision.gameObject.tag == "YellowCarAgent")
@@ -52,16 +53,45 @@ public class BlackCarAgent : Agent{
          }
        }
      
-
+*/
     //if an objects gets touched
     private void OnTriggerEnter(Collider other){
-        
+        GameObject car1 = this.gameObject;
+        GameObject car2 = other.gameObject;
+
+        GameObject test = this.transform.GetChild(2).gameObject;
+        GameObject test1 = this.transform.GetChild(3).gameObject;
+
+        //GameObject car1 = this.Collider;
+
+     
+        Debug.Log("Triggered Obj1: :" + car1.name);
+        Debug.Log("Triggered obj2: :" + car2.name);
+        Debug.Log("Child: :" + test.name);
+        Debug.Log("Child222: :" + test1.name);
+
+     //   Debug.Log("Triggered Obj1: :" + this.material.name);
+        Debug.Log("Material obj2: :" + other.material.name);
+        /*
+        if(car1.TryGetComponent<(car2.name == "frontbox")>(out RedCarAgent goalRed)){
+            Debug.Log("111111111111111111111111111");
+     
+
+        }
+
+*/
+        if (car2.name == "frontbox" && car1.name == "backbox"){
+            Debug.Log("YES!!!!");
+        }
+
+
+
         if (other.TryGetComponent<RedCarAgent>(out RedCarAgent goalRed)){
             SetReward(+1f);
             EndEpisode();
         }
         if (other.TryGetComponent<YellowCarAgent>(out YellowCarAgent goalYellow)){
-            Debug.Log(other.material.name);
+            //Debug.Log(other.material.name);
          
 
             SetReward(+1f);
