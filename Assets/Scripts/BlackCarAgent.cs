4cb6b4ad988356d207b49bbf47a92dc2ee1ab899
laryol <laryol2@gmx.de>
2022-02-04 19:20:55 +0100

    closeToFinal

--- Assets/Scripts/BlackCarAgent.cs
+++ Assets/Scripts/BlackCarAgent.cs
@@ -7,6 +7,7 @@ using Unity.MLAgents.Sensors;
 
 
 public class BlackCarAgent : Agent{
+    //Rigidbody rb;
     //init goal localPosition
     [SerializeField] private Transform targetTransformRed;
     [SerializeField] private Transform targetTransformYellow;
@@ -31,44 +32,62 @@ public class BlackCarAgent : Agent{
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-        float moveSpeed = 2f;
+        float moveSpeed = 8f;
         transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
 
-    /*//just for testing
+    //just for testing
     public override void Heuristic(in ActionBuffers actionsOut){
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
         continuousActions[0] = Input.GetAxisRaw("Horizontal");
         continuousActions[1] = Input.GetAxisRaw("Vertical");
-    }*/
+    }
 
     private float firstTime = -1f;
 
     //if an objects gets touched
     private void OnTriggerEnter(Collider other){
+      //  rb = GetComponent<Rigidbody>();
         //ich nehme mir die zeit wann jedes collider objekt eintrifft
-        //wenn die zeitabstände extrem klein sind (wie klein?), dann end episode
+        //wenn die zeitabstaende extrem klein sind (wie klein?), dann end episode
         float currentTime = Time.time;
         if(firstTime == currentTime){
             Debug.Log("YES");
-            EndEpisode();
+           // EndEpisode();
         }
         firstTime = currentTime;
+        if(other.material.name == "CarBox (Instance)"){
+           // other.attachedRigidbody.AddForce(new Vector3(-10,0,0));
+           other.attachedRigidbody.AddForce(-10,0,0,ForceMode.Impulse);
+                  //  Rigidbody rb;
+
+            Debug.Log("CARBOX");
+           // other.transform.parent.position = other.transform.position + new Vector3(0f,0f,1f);
+          //  other.attachedRigidbody.AddForce(-transform.forward);
+         // rb.AddForce(1,1,1);
+         //   other.rb.AddForce(-transform.forward);
+           // other.GetComponent<Rigidbody>().AddForce(new Vector3(10,10,10));
+         //  other.transform = rb.AddForce(transform.up * 40);
+         //   other.attachedRigidbody.AddForce(new Vector3(10,10,10));
+           // other.Rigidbody.velosity
+
+
+        }
 
         if(other.material.name == "Front (Instance)"){
             Debug.Log("No reward Front!");
-             EndEpisode();
+          //   EndEpisode();
         }
 
         if(other.material.name == "Back (Instance)"){
             SetReward(+1f);
             Debug.Log("Reward Back!");
-            EndEpisode();
+           // EndEpisode();
         }
 
         if (other.TryGetComponent<Wall>(out Wall wall)){
             SetReward(-5f);
-            EndEpisode();
+         //   EndEpisode();
         }
         /*if (other.TryGetComponent<RedGoal>(out RedGoal goalRed)){
             SetReward(+1f);
